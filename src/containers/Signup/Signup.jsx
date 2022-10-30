0c43f596ca2c525b5a18aace1c0af4fc7d0fdcc9
XdmriveroeX <104804272+XdmriveroeX@users.noreply.github.com>
2022-10-29 23:02:19 -0500

    Create-user-frontend

--- src/containers/Signup/Signup.jsx
+++ src/containers/Signup/Signup.jsx
@@ -1,11 +1,57 @@
+
 import './Signup.modules.css';
+import {useState} from "react";
+import Axios from 'axios';
 
 function Signup() {
-  return (
-    <div className="Signup-title">
-      <h1>Aqui el Signup</h1>
-    </div>
-  );
+
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [cellphoneNumber, setCellphoneNumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [accountType, setAccountType] = useState('');
+
+    const createUser = () => {
+        Axios.post('http://localhost:2022/v1/auth/user',
+            {name: name,
+                lastName: lastName,
+                email: email,
+                cellphoneNumber: cellphoneNumber,
+                password: password,
+                accountType: accountType}).then(() => {
+            console.log('success!');
+        })
+    };
+
+
+    return (
+        <div className="Signup-title">
+            <div className = 'informacion'>
+                <h1>Aqui el Signup</h1>
+                <label>Primer Nombre:</label>
+                <input type='text'
+                       onChange={(event)=>{setName(event.target.value);}}/>
+                <label>Apellido:</label>
+                <input type='text'
+                       onChange={(event)=>{setLastName(event.target.value);}}/>
+                <label>correo</label>
+                <input type='text'
+                       onChange={(event)=>{setEmail(event.target.value);}}/>
+                <label>numero celular:</label>
+                <input type='text'
+                       onChange={(event)=>{setCellphoneNumber(event.target.value);}}/>
+                <label>password:</label>
+                <input type='text'
+                       onChange={(event)=>{setPassword(event.target.value);}}/>
+                <label>tipo de cuenta</label>
+                <input type='text'
+                       onChange={(event)=>{setAccountType(event.target.value);}}/>
+                <button onClick={createUser}> Signup</button>
+            </div>
+        </div>
+
+    );
 }
 
 export default Signup;
