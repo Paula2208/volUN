2098b57705f8e097abf0cb96f54a20a563fb8c3c
XdmriveroeX <104804272+XdmriveroeX@users.noreply.github.com>
2022-11-09 17:27:40 -0500

    create/update/get/delete

--- src/api/offers.js
+++ src/api/offers.js
@@ -1,11 +1,59 @@
 import Axios from 'axios';
 
+export const createOferta = () => new Promise((resolve, reject) => {
+    Axios.post(`${process.env.REACT_APP_API_URL_V1}/offers/create`).then((results) => {
+        if(results.status === 200){
+            resolve(true);
+        }
+        else{
+            resolve(false);
+        }
+    })
+});
+
 export const getOfertas = () => new Promise((resolve, reject) => {
 
-    Axios.get(`${process.env.REACT_APP_API_URL_V1}/offers/get`).then((response) => {
-        console.log(response);
-    });
+    Axios.get(`${process.env.REACT_APP_API_URL_V1}/offers/get`).then((results) => {
+        
+        if(results.status === 200){
+            resolve(true);
+        }
+        else{
+            resolve(false);
+        }
+    })
+    .catch(err => {
+        reject(false);
+    })
 });
 
             
+export const deleteOferta = (id) => new Promise ((resolve, reject) => {
+    Axios.delete(`${process.env.REACT_APP_API_URL_V1}/offers/delete`,id).then((results) => {
+        if(results.status === 200){
+            resolve(true);
+        }
+        else{
+            resolve(false);
+        }
+    })
+    .catch(err => {
+        reject(false);
+    })
+    
+});
 
+export const updateOferta = (id) => new Promise ((resolve, reject) => {
+    Axios.put(`${process.env.REACT_APP_API_URL_V1}/offers/update`,3).then((results) => {
+        if(results.status === 200){
+            resolve(true);
+        }
+        else{
+            resolve(false);
+        }
+    })
+    .catch(err => {
+        reject(false);
+    })
+    
+});
--- src/containers/Feed/Feed.jsx
+++ src/containers/Feed/Feed.jsx
@@ -1,13 +1,16 @@
 import './Feed.modules.css';
 import Post from '../../components/post/Post';
 import { getOfertas } from "../../api/offers";
-import { useState } from "react"; 
-import Axios from "axios";
+/*import { MdTitle } from 'react-icons/md';*/
+import { deleteOferta } from "../../api/offers";
+import { createOferta } from "../../api/offers";
+import { updateOferta } from "../../api/offers";
+
 
 
 
 function Feed(props) {
-    const [listOfertas, setListaOfertas] = useState("");
+
     const {posts,userType} = props;
 
 
@@ -16,7 +19,11 @@ function Feed(props) {
             <div className="Feed-Scroll-container">
                 {posts.map((post) => <Post post={post} userType={userType}/>)}
                 <div className="ofertas">
+                <button onClick={createOferta}> create oferta</button>
                 <button onClick={getOfertas}> show ofertas</button>
+                <button onClick={deleteOferta}> delete oferta</button>
+                <button onClick={updateOferta}> update oferta</button>
+
                 </div>
             </div>
         </div>
