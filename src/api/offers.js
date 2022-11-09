62afc702e46370dafc50f7f7b67bf1b7cd30751d
XdmriveroeX <104804272+XdmriveroeX@users.noreply.github.com>
2022-11-09 17:38:02 -0500

    Revert "create/get/update/delete"

--- src/api/offers.js
+++ src/api/offers.js
@@ -1,59 +1,13 @@
 import Axios from 'axios';
 
-export const createOferta = () => new Promise((resolve, reject) => {
-    Axios.post(`${process.env.REACT_APP_API_URL_V1}/offers/create`).then((results) => {
-        if(results.status === 200){
-            resolve(true);
-        }
-        else{
-            resolve(false);
-        }
-    })
-});
+export const functionTemplate = () => new Promise((resolve, reject) => {
 
-export const getOfertas = () => new Promise((resolve, reject) => {
-
-    Axios.get(`${process.env.REACT_APP_API_URL_V1}/offers/get`).then((results) => {
-        
-        if(results.status === 200){
-            resolve(true);
-        }
-        else{
-            resolve(false);
-        }
-    })
-    .catch(err => {
-        reject(false);
-    })
-});
-
+    Axios.post(`${process.env.REACT_APP_API_URL_V1}/`, //add route and change method
+        {}) //add or delete body
+        .then((results) => {
             
-export const deleteOferta = (id) => new Promise ((resolve, reject) => {
-    Axios.delete(`${process.env.REACT_APP_API_URL_V1}/offers/delete`,id).then((results) => {
-        if(results.status === 200){
-            resolve(true);
-        }
-        else{
-            resolve(false);
-        }
-    })
-    .catch(err => {
-        reject(false);
-    })
-    
-});
-
-export const updateOferta = (id) => new Promise ((resolve, reject) => {
-    Axios.put(`${process.env.REACT_APP_API_URL_V1}/offers/update`,3).then((results) => {
-        if(results.status === 200){
-            resolve(true);
-        }
-        else{
-            resolve(false);
-        }
-    })
-    .catch(err => {
-        reject(false);
-    })
-    
+        })
+        .catch(err => {
+            
+        })
 });
