2262745c9aa360f196731d78763356b1fcf8c825
XdmriveroeX <104804272+XdmriveroeX@users.noreply.github.com>
2022-11-18 16:25:04 -0500

    Get number of non profits and posts

--- src/api/statistics.js
+++ src/api/statistics.js
@@ -11,3 +11,33 @@ export const functionTemplate = () => new Promise((resolve, reject) => {
             
         })
 });
+
+
+
+export const getNumberOfPosts = () => new Promise((resolve, reject) => {
+    Axios.get(`${process.env.REACT_APP_API_URL_V1}/statistics/getNumberOfPosts`).then((results) => {
+        if(results.status === 200){
+            resolve(results.data);
+        }
+        else{
+            resolve([]);
+        }
+    })
+    .catch(err => {
+        reject(false);
+    })
+});
+
+export const getNumberOfNonProfits = () => new Promise((resolve, reject) => {
+    Axios.get(`${process.env.REACT_APP_API_URL_V1}/statistics/getNumberOfNonProfits`).then((results) => {
+        if(results.status === 200){
+            resolve(results.data);
+        }
+        else{
+            resolve([]);
+        }
+    })
+    .catch(err => {
+        reject(false);
+    })
+});
