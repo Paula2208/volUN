47f730dc90016334140499814ad4b60a4036bcf7
XdmriveroeX <104804272+XdmriveroeX@users.noreply.github.com>
2022-11-24 11:39:23 -0500

    reports api

--- src/api/reports.js
+++ src/api/reports.js
@@ -11,3 +11,46 @@ export const functionTemplate = () => new Promise((resolve, reject) => {
             
         })
 });
+
+
+export const postReportsActive = (postId) => new Promise((resolve, reject) => {
+    Axios.get(`${process.env.REACT_APP_API_URL_V1}/reports/postReportsActive`),postId.then((results) => {
+        if(results.status === 200){
+            resolve(results.data);
+        }
+        else{
+            resolve([]);
+        }
+    })
+    .catch(err => {
+        reject(false);
+    })
+});
+
+export const postReportsPending = (postId) => new Promise((resolve, reject) => {
+    Axios.get(`${process.env.REACT_APP_API_URL_V1}/reports/postReportsPending`),postId.then((results) => {
+        if(results.status === 200){
+            resolve(results.data);
+        }
+        else{
+            resolve([]);
+        }
+    })
+    .catch(err => {
+        reject(false);
+    })
+});
+
+export const postReportsDenied = (postId) => new Promise((resolve, reject) => {
+    Axios.get(`${process.env.REACT_APP_API_URL_V1}/reports/postReportsDenied`),postId.then((results) => {
+        if(results.status === 200){
+            resolve(results.data);
+        }
+        else{
+            resolve([]);
+        }
+    })
+    .catch(err => {
+        reject(false);
+    })
+});
